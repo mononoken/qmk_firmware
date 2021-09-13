7547deab63748777d680e32014bc685a3c3c97a2
mononoken <ken.maeshima@pm.me>
2021-09-13 07:13:21 -0500

    Fixed encoder issue where right half was sending two outputs. Encoder action was defined in rev1 folder and in the keymap. Removed from keymap.

--- keyboards/splitkb/kyria/keymaps/mononoken/keymap.c
+++ keyboards/splitkb/kyria/keymaps/mononoken/keymap.c
@@ -73,20 +73,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     */
 };
-
-bool encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) { /* First encoder */
-        if (clockwise) {
-            tap_code(KC_PGDN);
-        } else {
-            tap_code(KC_PGUP);
-        }
-    } else if (index == 1) { /* Second encoder */
-        if (clockwise) {
-            tap_code(KC_1);
-        } else {
-            tap_code(KC_0);
-        }
-    }
-    return true;
-}
