689c8f1194b2575f8871d5d91381abd58c7da628
mononoken <ken.maeshima@pm.me>
2021-09-23 18:57:17 -0500

    Try to fix encoders

--- keyboards/splitkb/kyria/keymaps/mononoken/keymap.c
+++ keyboards/splitkb/kyria/keymaps/mononoken/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_ESC,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                                        KC_J,    KC_L,    KC_U,    KC_Y,    KC_EQL,  BASE,
       KC_TAB,  KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                                        KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    _______,
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    NAV,     KC_DEL,  NUMP,    FUN,     KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, _______,
-                                 _______, NUM,     LMOD,    KC_SPC,  KC_ENT,  _______, KC_BSPC, RMOD,    NUM,     _______
+                                 xxxxxxx, NUM,     LMOD,    KC_SPC,  KC_ENT,  _______, KC_BSPC, RMOD,    NUM,     xxxxxxx
     ),
     [_NUM] = LAYOUT(
       _______, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx,                                     xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, BASE,
