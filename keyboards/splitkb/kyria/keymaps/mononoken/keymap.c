27b07f6e207700dadd0588e1720a2708a53e9a2e
mononoken <ken.maeshima@pm.me>
2021-09-23 21:58:23 -0500

    fix some key layouts to include repeat, cancel, and clear

--- keyboards/splitkb/kyria/keymaps/mononoken/keymap.c
+++ keyboards/splitkb/kyria/keymaps/mononoken/keymap.c
@@ -30,14 +30,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
       KC_ESC,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                                        KC_J,    KC_L,    KC_U,    KC_Y,    KC_EQL,  BASE,
       KC_TAB,  KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                                        KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    _______,
-      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    NAV,     KC_DEL,  NUMP,    FUN,     KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, _______,
-                                 xxxxxxx, NUM,     LMOD,    KC_SPC,  KC_ENT,  _______, KC_BSPC, RMOD,    NUM,     xxxxxxx
+      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    NUM,     KC_DEL,  NUMP,    FUN,     KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, _______,
+                                 xxxxxxx, NAV,     LMOD,    KC_SPC,  KC_ENT,  KC_BSPC, REPEAT, RMOD,    NUM,     xxxxxxx
     ),
     [_NUM] = LAYOUT(
       _______, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx,                                     xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, BASE,
       _______, KC_6,    KC_4,    KC_0,    KC_2,    xxxxxxx,                                     xxxxxxx, KC_3,    KC_1,    KC_5,    KC_7,    _______,
       _______, xxxxxxx, xxxxxxx, xxxxxxx, KC_8,    xxxxxxx, _______, _______, _______, _______, xxxxxxx, KC_9,    xxxxxxx, xxxxxxx, xxxxxxx, _______,
-                                 _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+                                 _______, _______, _______, _______, _______, _______, CANCEL,  _______, _______, _______
     ),
     [_NAV] = LAYOUT(
       _______, _______, _______, KC_UP,   KC_PGUP, KC_HOME,                                     _______, _______, _______, _______, _______, BASE,
@@ -48,15 +48,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Important that the symbols on the base layer have the same positions as these symbols
     [_LMOD] = LAYOUT(
       _______, OPT,     C(KC_W), C(KC_A), _______, _______,                                     _______, KC_HASH, KC_DLR,  KC_AT,   KC_SCLN, BASE,
-      _______, OS_ALT,  OS_SHFT,  OS_CTRL,  OS_GUI,  _______,                                     KC_DQT,  KC_QUOT, KC_LBRC, KC_RBRC, KC_COLN, _______,
+      _______, OS_ALT,  OS_SHFT, OS_CTRL, OS_GUI,  _______,                                     KC_DQT,  KC_QUOT, KC_LBRC, KC_RBRC, KC_COLN, _______,
       _______, C(KC_Z), C(KC_X), C(KC_C), C(KC_V), C(KC_V), _______, _______, _______, _______, KC_CIRC, KC_AMPR, KC_LPRN, KC_RPRN, KC_QUES, _______,
-                                 _______, _______, _______, _______, _______, _______, BASE,    FUN,     _______, _______
+                                 _______, _______, _______, _______, _______, _______, CLEAR,   FUN,     _______, _______
     ),
     [_RMOD] = LAYOUT(
       _______, KC_TILD, KC_PLUS, KC_ASTR, KC_EXLM, _______,                                     _______, _______, _______, _______, _______, BASE,
-      _______, KC_PIPE, KC_LCBR, KC_RCBR, KC_MINS, KC_GRV,                                      _______, OS_GUI,  OS_CTRL,  OS_SHFT,  OS_ALT,  _______,
-      _______, KC_BSLS,  KC_LABK, KC_RABK, KC_UNDS,KC_PERC, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-                                 _______, _______, FUN,     _______, _______, _______, BASE,    _______, _______, _______
+      _______, KC_PIPE, KC_LCBR, KC_RCBR, KC_MINS, KC_GRV,                                      _______, OS_GUI,  OS_CTRL, OS_SHFT, OS_ALT,  _______,
+      _______, KC_BSLS, KC_LABK, KC_RABK, KC_UNDS, KC_PERC, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+                                 _______, _______, FUN,     _______, _______, _______, CLEAR,   _______, _______, _______
     ),
     [_FUN] = LAYOUT(
       _______, _______, _______, _______, _______, _______,                                     _______, _______, _______, _______, _______, BASE,
