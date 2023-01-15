36cec1325543661a509d6c1ac6328105e4427e3f
mononoken <ken.maeshima@pm.me>
2023-01-15 13:24:19 -0800

    Move key and layer defs to keycodes.h

---
+++ keyboards/splitkb/kyria/keymaps/mononoken/keycodes.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include QMK_KEYBOARD_H
+
+#include "quantum.h"
+
+enum layers {
+    _BASE = 0,
+    _NAVI,
+    _LSYM,
+    _RSYM,
+    _SHRT,
+    _NUM,
+    _FUNC,
+};
+
+#define BASE  DF(_BASE)
+#define LSYM  MO(_LSYM)
+#define RSYM  MO(_RSYM)
+#define SHRT  OSL(_SHRT)
+#define FUNC  MO(_SHRT)
+
+#define xxxxxxx KC_NO
+
+#define MT_SPC  LT(_NAVI, KC_SPC)
+#define C_TAB   G(KC_TAB)
+#define CS_TAB  SGUI(KC_TAB)
+#define DN_CTRL MT(MOD_LCTL, KC_DOWN)
+
+#define OS_CTRL  OSM(MOD_LCTL)
+#define OS_SHFT  OSM(MOD_LSFT)
+#define OS_ALT   OSM(MOD_LALT)
+#define OS_GUI   OSM(MOD_LGUI)
--- keyboards/splitkb/kyria/keymaps/mononoken/keymap.c
+++ keyboards/splitkb/kyria/keymaps/mononoken/keymap.c
@@ -15,35 +15,7 @@
  */
 #include QMK_KEYBOARD_H
 
-enum layers {
-    _BASE = 0,
-    _NAVI,
-    _LSYM,
-    _RSYM,
-    _SHRT,
-    _NUM,
-    _FUNC,
-    // _COLE,
-};
-
-// Aliases for readability
-#define xxxxxxx KC_NO
-
-#define BASE  DF(_BASE)
-#define LSYM  MO(_LSYM)
-#define RSYM  MO(_RSYM)
-#define SHRT  OSL(_SHRT)
-#define FUNC  MO(_SHRT)
-
-#define MT_SPC  LT(_NAVI, KC_SPC)
-#define C_TAB   G(KC_TAB)
-#define CS_TAB  SGUI(KC_TAB)
-#define DN_CTRL MT(MOD_LCTL, KC_DOWN)
-
-#define OS_CTRL  OSM(MOD_LCTL)
-#define OS_SHFT  OSM(MOD_LSFT)
-#define OS_ALT   OSM(MOD_LALT)
-#define OS_GUI   OSM(MOD_LGUI)
+#include "keycodes.h"
 
 #include "g/keymap_combo.h"
 
