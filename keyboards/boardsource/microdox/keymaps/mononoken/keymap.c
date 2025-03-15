bd5f0264cf6a133bfc8225a0b33d50f01d5d182a
mononoken <81536479+mononoken@users.noreply.github.com>
2025-03-15 16:57:21 -0700

    Swap y and rprn

--- keyboards/boardsource/microdox/keymaps/mononoken/keymap.c
+++ keyboards/boardsource/microdox/keymaps/mononoken/keymap.c
@@ -40,9 +40,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * Base Layer: Modified RSTHD/treeman
      */
     [_BASE] = LAYOUT_split_3x5_3(
-      KC_UNDS, KC_C,    KC_F,    KC_K,    KC_B,         KC_X,    KC_J,    KC_O,    KC_U,    KC_Y,
+      KC_UNDS, KC_C,    KC_F,    KC_K,    KC_B,         KC_X,    KC_J,    KC_O,    KC_U,    KC_RPRN,
       KC_R,    KC_S,    KC_T,    KC_H,    KC_P,         KC_M,    KC_N,    KC_A,    KC_I,    REPEAT,
-      KC_V,    KC_W,    KC_G,    KC_D,    KC_LPRN,      KC_SLSH, KC_L,    KC_COMM, KC_DOT,  KC_RPRN,
+      KC_V,    KC_W,    KC_G,    KC_D,    KC_LPRN,      KC_SLSH, KC_L,    KC_COMM, KC_DOT,  KC_Y,
                         xxxxxxx, LSYM,    MT_SPC,       KC_E,    RSYM,    xxxxxxx
     ),
     [_NUM]  = LAYOUT_split_3x5_3(
@@ -53,21 +53,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     // Important that the symbols on the base layer have the same positions as these symbols
     [_SYM]  = LAYOUT_split_3x5_3(
-      KC_UNDS, KC_PLUS, KC_ASTR, KC_EXLM, xxxxxxx,      xxxxxxx, KC_HASH, KC_AT,   REPEAT,  KC_PERC,
+      KC_UNDS, KC_PLUS, KC_ASTR, KC_EXLM, xxxxxxx,      xxxxxxx, KC_HASH, KC_AT,   REPEAT,  KC_RPRN,
       CIRC,    KC_LCBR, KC_RCBR, TILD,    xxxxxxx,      KC_BSLS, GRV,     KC_LBRC, KC_RBRC, KC_DLR,
-      KC_PERC, KC_LABK, KC_RABK, KC_PIPE, KC_LPRN,      KC_SLSH, KC_AMPR, KC_COMM, KC_DOT,  KC_RPRN,
+      KC_PERC, KC_LABK, KC_RABK, KC_PIPE, KC_LPRN,      KC_SLSH, KC_AMPR, KC_COMM, KC_DOT,  KC_PERC,
                         xxxxxxx, _______, _______,      CANCEL,  _______, xxxxxxx
     ),
     [_LSYM]  = LAYOUT_split_3x5_3(
-      KC_UNDS, KC_PLUS, KC_ASTR, KC_EXLM, xxxxxxx,      KC_X,    KC_J,    AT_U,    REPEAT,  KC_PERC,
+      KC_UNDS, KC_PLUS, KC_ASTR, KC_EXLM, xxxxxxx,      KC_X,    KC_J,    AT_U,    REPEAT,  KC_RPRN,
       CIRC,    KC_LCBR, KC_RCBR, TILD,    xxxxxxx,      KC_BSLS, KC_3,    KC_1,    KC_5,    KC_7,
-      KC_PERC, KC_LABK, KC_RABK, KC_PIPE, KC_LPRN,      KC_SLSH, KC_9,    KC_COMM, KC_DOT,  KC_RPRN,
+      KC_PERC, KC_LABK, KC_RABK, KC_PIPE, KC_LPRN,      KC_SLSH, KC_9,    KC_COMM, KC_DOT,  KC_PERC,
                         xxxxxxx, _______, _______,      CANCEL,  NUM,     xxxxxxx
     ),
     [_RSYM]  = LAYOUT_split_3x5_3(
-      _______, KC_PLUS, KC_ASTR, KC_K,    xxxxxxx,      xxxxxxx, KC_HASH, KC_AT,   REPEAT,  KC_PERC,
+      _______, KC_PLUS, KC_ASTR, KC_K,    xxxxxxx,      xxxxxxx, KC_HASH, KC_AT,   REPEAT,  KC_RPRN,
       KC_6,    KC_4,    KC_0,    KC_2,    KC_MINS,      KC_BSLS, GRV,     KC_LBRC, KC_RBRC, KC_DLR,
-      KC_PERC, xxxxxxx, NUM_G,   KC_8,    KC_LPRN,      KC_SLSH, KC_AMPR, KC_COMM, KC_DOT,  KC_RPRN,
+      KC_PERC, xxxxxxx, NUM_G,   KC_8,    KC_LPRN,      KC_SLSH, KC_AMPR, KC_COMM, KC_DOT,  KC_PERC,
                         xxxxxxx, NUM,     _______,      CANCEL,  _______, xxxxxxx
     ),
     [_NAV]  = LAYOUT_split_3x5_3(
